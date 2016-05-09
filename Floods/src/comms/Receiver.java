c2004cf2766aa9de9ed1d9acf9ff5e77bc84640e
Huw McNamara <hm649@york.ac.uk>
2016-05-09 16:17:18 +0100

    basic send rec

--- Floods/src/comms/Receiver.java
+++ Floods/src/comms/Receiver.java
@@ -1,8 +1,10 @@
 package comms;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
@@ -16,54 +18,86 @@ public class Receiver {
 
 	private final int PORT = 5555;
 	private final int SIZE_BYTES = 4;
-	
-	public Receiver() {}
+
+	public Receiver() {
+	}
 
 	private void recData() {
 		try (ServerSocket ss = new ServerSocket(PORT);
 				Socket cs = ss.accept();
 				BufferedInputStream in = new BufferedInputStream(cs.getInputStream())) {
 
-			ByteBuffer br = ByteBuffer.allocateDirect(SIZE_BYTES);
-			br.order(ByteOrder.BIG_ENDIAN);
-			
+			int size = 0;
 			for (int i = 0; i != SIZE_BYTES; ++i) {
 				int readByte = in.read();
 				if (readByte == -1) {
 					throw new Exception();
 				}
-				br.put((byte) readByte);
-				
+				size |= readByte << 8 * (SIZE_BYTES - i - 1);
+
 			}
-			
-			
-			int msgLength = br.getInt();
-			
+
+			int msgLength = size;
+
 			byte[] byteBuf = new byte[msgLength];
-			
+
 			int bytesRead = in.read(byteBuf, 0, msgLength);
-			
+
 			if (bytesRead != msgLength) {
 				throw new Exception();
 			}
-			
+
 			Gson gson = new Gson();
-			
+
 			String decodedStr = gson.fromJson(new String(byteBuf, "UTF-8"), String.class);
-			
+
 			System.out.println(decodedStr);
-			
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	private void sendData() {
+
+		try (ServerSocket ss = new ServerSocket(PORT);
+				Socket cs = ss.accept();
+				BufferedOutputStream out = new BufferedOutputStream(cs.getOutputStream())) {
+
+			String toSend = "Hello";
+
+			Gson gson = new Gson();
+
+			String encodedStr = gson.toJson(toSend);
+
+			try {
+				byte[] byteArr = encodedStr.getBytes("UTF-8");
+				int size = byteArr.length;
+
+				out.write(0);
+				out.write(0);
+				out.write(0);
+				out.write(size);
+
+				out.write(byteArr, 0, size);
+
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public static void main(String[] args) {
 
-
 		Receiver rec = new Receiver();
-		rec.recData();
+		rec.sendData();
 
 	}
 
