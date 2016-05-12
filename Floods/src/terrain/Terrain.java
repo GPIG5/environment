150c4948d473f10d2fd9f787983171b2646b026e
David Wotherspoon <david@dwotherspoon.com>
2016-05-12 17:02:38 +0100

    Working OpenCL implementation.

--- Floods/src/terrain/Terrain.java
+++ Floods/src/terrain/Terrain.java
@@ -6,7 +6,7 @@ import java.util.PriorityQueue;
 
 import com.jme3.material.Material;
 
-import water.Cell;
+import water.Cells;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Quad;
@@ -28,70 +28,8 @@ public class Terrain {
         ncols = ater.getCols();
 	}
 
-	public Cell[] makeCells() {
-		System.out.println("Making water cells...");
-		Vector3f vertices[] = ater.getVertices();
-		Cell[] result =  new Cell[(nrows-1)*(ncols-1)];
-		float csize = ater.getCellsize() * scale;
-		Quad q = new Quad(csize, csize);
-		int i = 0;
-		for (int r = 0; r < nrows-1; r++) {
-			//System.out.println("Row: " + r);
-			for (int c = 0; c < ncols-1; c++) {
-				int base = (r * ncols) + c;
-				Vector3f v1 = vertices[base].mult(scale);
-				v1.addLocal(g.getWorldTranslation());
-				Vector3f v2 = vertices[base + 1].mult(scale);
-				v2.addLocal(g.getWorldTranslation());
-				Vector3f v3 = vertices[base + ncols].mult(scale);
-				v3.addLocal(g.getWorldTranslation());
-				Vector3f v4 = vertices[base + ncols + 1].mult(scale);
-				v4.addLocal(g.getWorldTranslation());
-				result[i] = new Cell(v1, v2, v3, v4, csize);
-				result[i].setMesh(q);
-				i++;
-			}
-		}
-		System.out.println("Making pipes...");
-		// Run through again and set neighbours. 
-		// n, e, s, w
-		// Do corners.
-		// TL
-		result[0].setPipes(
-				null, result[1], result[ncols-1], null);
-		// TR
-		result[ncols-2].setPipes(
-				null, null, result[(ncols-1) + (ncols-2)], result[ncols-3]);
-		// BL
-		result[(ncols-1)*(nrows-2)].setPipes(
-				result[(ncols-1)*(nrows-3)], result[(ncols-1)*(nrows-2) + 1], null, null);
-		// BR
-		result[(ncols-1)*(nrows-2) + (ncols-2)].setPipes(
-				result[(ncols-1)*(nrows-3) + (ncols-2)], null, null, result[(ncols-1)*(nrows-2) + (ncols-3)]);
-		// First + last row (not corners)
-		for (int c = 1; c < ncols-2; c++) {
-			// First row
-			result[c].setPipes(
-					null, result[c+1], result[(ncols-1) + c], result[c-1]);
-			// Last row
-			result[(ncols-1)*(nrows-2) + c].setPipes(
-					result[(ncols-1)*(nrows-3) + c], result[(ncols-1)*(nrows-2) + (c+1)], null, result[(ncols-1)*(nrows-2) + (c-1)]);
-		}
-		// First + last column (not corners)
-		for (int r = 1; r < nrows-2; r++) {
-			// First col
-			result[(ncols-1)*r].setPipes(
-					result[(ncols-1)*(r-1)], result[(ncols-1)*r + 1], result[(ncols-1)*(r+1)], null);
-			// Last col
-			result[(ncols-1)*r + (ncols-2)].setPipes(
-					result[(ncols-1)*(r-1) + (ncols-2)], null, result[(ncols-1)*(r+1) + (ncols-2)], result[(ncols-1)*r + (ncols-3)]);
-			// Do centre.
-			for (int c = 1; c < ncols-2; c++) {
-				result[(ncols-1)*r + c].setPipes(
-						result[(ncols-1)*(r-1) + c], result[(ncols-1)*r + (c+1)], result[(ncols-1)*(r+1) + c], result[(ncols-1)*r + (c-1)]);
-			}
-		}
-		return result;
+	public Cells makeCells() {
+		return new Cells(ater.getVertices(), nrows, ncols, ater.getCellsize(), scale);
 	}
 	
 	public int getRows() {
--- Floods/src/water/Water.java
+++ Floods/src/water/Water.java
@@ -25,20 +25,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
 import java.util.List;
 
 import static org.lwjgl.opencl.CL10.*;
-import static org.lwjgl.opencl.CL11.*;
 
-import terrain.ASCTerrain;
 import terrain.Terrain;
 
 public class Water extends Node {
 	long ticks_last;
 	// Group particles by the row they occupy.
-	Cell[] grid;
+	Cells cells;
 	Terrain terrain;
-	int ncells;
+	int size;
+	float csize2;
+	Geometry[] planes;
+	Vector3f[] points;
 	// OpenCL variables
 	public static CLContext context;
 	public static CLPlatform platform;
@@ -46,6 +48,22 @@ public class Water extends Node {
 	public static CLCommandQueue queue;
 	public static CLKernel calcFlow;
 	public static CLKernel calcHeight;
+	private static CLMem hMem;
+	private static CLMem pMem;
+	private static CLMem fMem;
+	private static CLMem tMem;
+	
+	// Work memory
+	// Height buffer
+	FloatBuffer hBuff;
+	// Terrain height buffer
+	FloatBuffer tBuff;
+	// Pipes buffer
+	IntBuffer pBuff;
+	// Flows buffer
+	FloatBuffer fBuff;
+	// Result buffer
+	FloatBuffer rBuff;
 	
 	// https://github.com/LWJGL/lwjgl/blob/master/src/java/org/lwjgl/test/opencl/HelloOpenCL.java
 	public Water(Terrain t, Material mat) {
@@ -53,12 +71,17 @@ public class Water extends Node {
 		// Params
 		ticks_last = System.currentTimeMillis();
 		terrain = t;
-		grid = terrain.makeCells();
-		ncells = grid.length;
+		cells = terrain.makeCells();
+		size = cells.getSize();
+		csize2 = cells.getCsize2();
+		planes = cells.getPlanes();
+		points = cells.getPoints();
 		System.out.println("Adding water cells to scene...");
-		for (int c = 0; c < ncells; c++) {
-			grid[c].setMaterial(mat);
-			attachChild(grid[c]);
+		for (Geometry g : cells.getPlanes()) {
+			g.setMaterial(mat);
+			// Cull initially
+			attachChild(g);
+			g.setCullHint(cullHint.Always);
 		}
 		// OpenCL
 		try {
@@ -73,21 +96,44 @@ public class Water extends Node {
 				context = CLContext.create(platform, devices, errorBuf);
 				// Create a command queue
 				queue = CL10.clCreateCommandQueue(context, devices.get(0), CL10.CL_QUEUE_PROFILING_ENABLE, errorBuf);
-				// Check for any errors
 				Util.checkCLError(errorBuf.get(0)); 
-				CLProgram prog = CL10.clCreateProgramWithSource(context, loadCLProgram(), null);
 				// Build the OpenCL program, store it on the specified device
+				CLProgram prog = CL10.clCreateProgramWithSource(context, loadCLProgram(), null);
 				int error = CL10.clBuildProgram(prog, devices.get(0), "", null);
 				System.out.println(prog.getBuildInfoString(devices.get(0), CL_PROGRAM_BUILD_LOG));
 				// Check for any OpenCL errors
 				Util.checkCLError(error);
+				// Kernels
+				calcFlow = CL10.clCreateKernel(prog, "flow", null);
+				calcHeight = CL10.clCreateKernel(prog, "height", null);
+				// Memory
+				hBuff = BufferUtils.createFloatBuffer(size);
+				hBuff.put(cells.getHeights());
+				hBuff.rewind();
+				hMem = CL10.clCreateBuffer(context, CL10.CL_MEM_READ_WRITE | CL10.CL_MEM_COPY_HOST_PTR, hBuff, errorBuf);
+				
+				tBuff = BufferUtils.createFloatBuffer(size);
+				tBuff.put(cells.getTerHeights());
+				tBuff.rewind();
+				tMem = CL10.clCreateBuffer(context, CL10.CL_MEM_READ_ONLY | CL10.CL_MEM_COPY_HOST_PTR, tBuff, errorBuf);
+				
+				pBuff = BufferUtils.createIntBuffer(size<<2);
+				pBuff.put(cells.getPipes());
+				pBuff.rewind();
+				pMem = CL10.clCreateBuffer(context, CL10.CL_MEM_READ_ONLY | CL10.CL_MEM_COPY_HOST_PTR, pBuff, errorBuf);
+				
+				fBuff = BufferUtils.createFloatBuffer(size<<2);
+				fBuff.put(cells.getFlows());
+				fBuff.rewind();
+				fMem = CL10.clCreateBuffer(context, CL10.CL_MEM_READ_WRITE | CL10.CL_MEM_COPY_HOST_PTR, fBuff, errorBuf);
+				
+				rBuff = BufferUtils.createFloatBuffer(size);
+				
 			}
 		} catch (LWJGLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		//grid[20][20].setVoume(1);
 	}
 	
 	private String loadCLProgram() {
@@ -110,16 +156,46 @@ public class Water extends Node {
 	}
 	
 	public void process() {
-		grid[20000].add(0.01f);
 		long ticks_now = System.currentTimeMillis();
 		// How much time this frame represents.
 		float t = (ticks_now - ticks_last)/1000.0f;
 		ticks_last = ticks_now;
+		calcFlow.setArg(0, t);
+		calcFlow.setArg(1, csize2);
+		calcFlow.setArg(2, hMem);
+		calcFlow.setArg(3, tMem);
+		calcFlow.setArg(4, fMem);
+		calcFlow.setArg(5, pMem);
+		final int dimensions = 1; 
+		PointerBuffer globalWorkSize = BufferUtils.createPointerBuffer(dimensions);
+		globalWorkSize.put(0, size);
+		// Run the specified number of work units using our OpenCL program kernel
+		CL10.clEnqueueNDRangeKernel(queue, calcFlow, dimensions, null, globalWorkSize, null, null, null);
+		CL10.clFinish(queue);
+		calcHeight.setArg(0, t);
+		calcHeight.setArg(1, csize2);
+		calcHeight.setArg(2, hMem);
+		calcHeight.setArg(3, fMem);
+		calcHeight.setArg(4, pMem);
+		CL10.clEnqueueNDRangeKernel(queue, calcHeight, dimensions, null, globalWorkSize, null, null, null);
+		CL10.clFinish(queue);
+		// Read the new heights
+		rBuff.rewind();
+		CL10.clEnqueueReadBuffer(queue, hMem, CL10.CL_TRUE, 0, rBuff, null, null);
+		for (int i = 0; i < rBuff.capacity(); i++) {
+			planes[i].setLocalTranslation(points[i].x, rBuff.get(i) + tBuff.get(i), points[i].z);
+			// Cull until 0.001 surpassed.
+			if (rBuff.get(i) > 0.001) {
+				planes[i].setCullHint(cullHint.Dynamic);
+			}
+		}
+		/*
 		for (int c = 0; c < ncells; c++) {
 				grid[c].flow(t*20);
 		}
 		for (int c = 0; c < ncells; c++) {
 			grid[c].redraw(t*20);
 		}
+		*/
 	}
 }
