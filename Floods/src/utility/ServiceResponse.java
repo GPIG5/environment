ae3f8a41a0c41dc40ab7e802952f427cfbf84cd4
David Wotherspoon <david@dwotherspoon.com>
2016-05-16 15:12:31 +0100

    Add getters and setters.

--- Floods/src/utility/ServiceResponse.java
+++ Floods/src/utility/ServiceResponse.java
@@ -1,9 +1,25 @@
 package utility;
 
-public class ServiceResponse extends Message {
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
-	public ServiceResponse(String uuid) {
+import mesh.PINOR;
+
+public class ServiceResponse extends Message {
+	ArrayList<PINOR> pinors;
+	BufferedImage image;
+	
+	public ServiceResponse(String uuid, ArrayList<PINOR> pinors, BufferedImage image) {
 		super(uuid);
-		// TODO Auto-generated constructor stub
+		this.pinors = pinors;
+		this.image = image;
+	}
+	
+	public ArrayList<PINOR> getPinors() {
+		return pinors;
+	}
+	
+	public BufferedImage getImage() {
+		return image;
 	}
 }
