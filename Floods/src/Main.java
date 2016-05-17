0b6fc32b0084e19d87bcb36c8114c9aede113764
David Wotherspoon <david@dwotherspoon.com>
2016-05-17 13:22:43 +0100

    OSGB to WGS84 prototype.

--- Floods/src/Main.java
+++ Floods/src/Main.java
@@ -1,5 +1,11 @@
+import org.opengis.geometry.MismatchedDimensionException;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.opengis.referencing.operation.TransformException;
+
 import mesh.Mesh;
 import simulation.Simulation;
+import utility.Location;
 import utility.ServiceInterface;
 
 public class Main {
@@ -10,6 +16,22 @@ public class Main {
         mesh.start(si);
         Simulation sim = new Simulation();
         sim.start(si);
+        Location loc = new Location(54.007111f, -1.004663f, 0);
+        try {
+            loc.getOSGB();
+        } catch (MismatchedDimensionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchAuthorityCodeException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (FactoryException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (TransformException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } */
     }
 
 }
--- Floods/src/utility/Location.java
+++ Floods/src/utility/Location.java
@@ -1,5 +1,19 @@
 package utility;
 
+import org.geotools.geometry.GeneralDirectPosition;
+import org.geotools.referencing.ReferencingFactoryFinder;
+import org.geotools.referencing.operation.DefaultCoordinateOperationFactory;
+import org.opengis.geometry.DirectPosition;
+import org.opengis.geometry.MismatchedDimensionException;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.opengis.referencing.crs.CRSAuthorityFactory;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.CoordinateOperation;
+import org.opengis.referencing.operation.TransformException;
+
+// import org.geotools.referencing
+
 /**
  * Created by hm649 on 16/05/16.
  */
@@ -52,5 +66,21 @@ public class Location {
 
     public void setAlt(float alt) {
         this.alt = alt;
+    }
+    
+    // http://stackoverflow.com/questions/4313618/convert-latitude-and-longitude-to-northing-and-easting-in-java
+    public void getOSGB() throws NoSuchAuthorityCodeException, FactoryException, MismatchedDimensionException, TransformException {
+        CRSAuthorityFactory crsfac = ReferencingFactoryFinder.getCRSAuthorityFactory("EPSG", null);
+        // 27700 is the EPSG code for OSGB36
+        CoordinateReferenceSystem osgbcrs = crsfac.createCoordinateReferenceSystem("27700");
+        // 4326 is the EPSG code for WGS84 aka Lat-Lng
+        CoordinateReferenceSystem wgs84crs = crsfac.createCoordinateReferenceSystem("4326");
+        
+        CoordinateOperation op = new DefaultCoordinateOperationFactory().createOperation(wgs84crs, osgbcrs);
+
+        DirectPosition latLng = new GeneralDirectPosition(this.lat, this.lon);
+        DirectPosition osgb = op.getMathTransform().transform(latLng, latLng);
+        System.out.println("X: " + osgb.getOrdinate(0));
+        System.out.println("Y: " + osgb.getOrdinate(1));
     }
 }
