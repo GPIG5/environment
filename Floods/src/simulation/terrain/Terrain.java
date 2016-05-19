967bba36b0de52dbb478887fae2ca2553fef17d2
David Wotherspoon <david@dwotherspoon.com>
2016-05-19 13:49:37 +0100

    Remove uneccesary prints.

--- Floods/src/simulation/terrain/Terrain.java
+++ Floods/src/simulation/terrain/Terrain.java
@@ -47,11 +47,9 @@ public class Terrain {
     }
 
     public Vector3f osgbTo3D(DirectPosition osgb, float alt) {
-        System.out.println("osgb0: " + osgb.getOrdinate(0) + " osgb1: " + osgb.getOrdinate(1));
         Vector3f loc = new Vector3f(((float) osgb.getOrdinate(1) - ater.getYll()) * scale,
                 3 * alt * scale,
                 ((float) osgb.getOrdinate(0) - ater.getXll()) * scale);
-        System.out.println(loc);
         return loc;
     }
 
